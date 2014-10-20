f43fe66966636759e2272c870b1d8afc927a3a1a
Parva Thakkar <parva.thakkar@gmail.com>
2014-10-19 20:51:24 -0400

    works! android studio format

--- app/src/main/java/course/labs/intentslab/ExplicitlyLoadedActivity.java
+++ app/src/main/java/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -46,13 +46,18 @@ public class ExplicitlyLoadedActivity extends Activity {
 
 		Log.i(TAG,"Entered enterClicked()");
 		
-		// TODO - Save user provided input from the EditText field
+		// Save user provided input from the EditText field
+        String resultText = mEditText.getText().toString();
 
-		// TODO - Create a new intent and save the input from the EditText field as an extra
+		// Create a new intent and save the input from the EditText field as an extra
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("result", resultText);
 		
-		// TODO - Set Activity's result with result code RESULT_OK
-		
-		// TODO - Finish the Activity
+		// Set Activity's result with result code RESULT_OK
+		setResult(RESULT_OK, resultIntent);
+
+		// Finish the Activity
+        finish();
 
 	}
 }
